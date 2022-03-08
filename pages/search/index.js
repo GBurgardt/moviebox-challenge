09b0186ff41c2b5864473e3e24412cc9f50497dd
German Burgardt <germanburgardt@192.168.1.5>
2022-03-08 15:06:13 -0300

    sort movies

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,6 +1,7 @@
 import { Container, ImageList } from '@mui/material'
-import { getMovies } from '../../lib/api/api';
+import { getMovies, getMovie } from '../../lib/api/api';
 import ImageItem from '../../components/Search/ImageItem';
+import { getParsedRating, getParsedRatingAverage } from '../../lib/utils/utils';
 
 export default function Search({ response }) {
     const results = response.results || [];
@@ -23,9 +24,32 @@ export default function Search({ response }) {
     )
 }
 
-export const getServerSideProps = async (context) => ({
-    props: {
-        response: context.query.q && context.query.q.length > 3 ? await getMovies(context.query.q) : []
+export const getServerSideProps = async (context) => {
+
+    const movies = await getMovies(context.query.q);
+
+    const detailsMoviesPromises = movies.results.map(
+        m => getMovie(m.imdbID)
+    );
+
+    const detailsMovies = await Promise.all(detailsMoviesPromises);
+
+    const results = detailsMovies
+        .map(d => d.results)
+        .sort((a, b) => {
+            const ratingsA = getParsedRating(a.Ratings);
+            const ratingsB = getParsedRating(b.Ratings);
+
+            const averageA = getParsedRatingAverage(ratingsA);
+            const averageB = getParsedRatingAverage(ratingsB);
+
+            return averageB - averageA;
+        });
+
+    return {
+        props: {
+            response: context.query.q && context.query.q.length > 3 ? { results } : []
+        }
     }
-})
+}
 
